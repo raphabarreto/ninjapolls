ecd8569ca213ee0823157d531495fadf18221e36
Raphael Barreto <tnt.raphael@gmail.com>
2020-04-18 23:47:25 -0300

    Header & Footer Components

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,17 @@
 <script>
-
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
 </script>
 
 <style>
-
+  main {
+    max-width: 960px;
+    margin: 40px auto;
+  }
 </style>
 
-<main>
-  <h1>Hello, ninjas!</h1>
-</main>
+<Header />
+
+<main />
+
+<Footer />
