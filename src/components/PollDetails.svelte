343bb574f1350ee815f23e8e5cec6b1cd30c8c0e
Raphael Barreto <tnt.raphael@gmail.com>
2020-04-26 12:59:11 -0300

    Card Component

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Card from "../shared/Card.svelte";
   export let poll;
 
   // reactive values
@@ -35,15 +36,17 @@
   }
 </style>
 
-<div class="poll">
-  <h3>{poll.question}</h3>
-  <p>Total votes: {totalVotes}</p>
-  <div class="answer">
-    <div class="percent percent-a" />
-    <span>{poll.answerA} ({poll.votesA})</span>
+<Card>
+  <div class="poll">
+    <h3>{poll.question}</h3>
+    <p>Total votes: {totalVotes}</p>
+    <div class="answer">
+      <div class="percent percent-a" />
+      <span>{poll.answerA} ({poll.votesA})</span>
+    </div>
+    <div class="answer">
+      <div class="percent percent-b" />
+      <span>{poll.answerB} ({poll.votesB})</span>
+    </div>
   </div>
-  <div class="answer">
-    <div class="percent percent-b" />
-    <span>{poll.answerB} ({poll.votesB})</span>
-  </div>
-</div>
+</Card>
---
+++ src/shared/Card.svelte
@@ -0,0 +1,12 @@
+<style>
+  .card {
+    background: #fff;
+    padding: 20px;
+    border-radius: 6px;
+    box-shadow: 0px 1px 4px 2px rgba(0, 0, 0, 0.3);
+  }
+</style>
+
+<div class="card">
+  <slot />
+</div>
