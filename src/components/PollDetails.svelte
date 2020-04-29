178f98c5c18e2782d9725d20026f4e5df62a07f8
Raphael Barreto <tnt.raphael@gmail.com>
2020-04-29 18:26:02 -0300

    Updating Store Data

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -1,8 +1,7 @@
 <script>
-  import { createEventDispatcher } from "svelte";
   import Card from "../shared/Card.svelte";
+  import PollStore from "../stores/PollStore.js";
   export let poll;
-  const dispatch = createEventDispatcher();
 
   // reactive values
   $: totalVotes = poll.votesA + poll.votesB;
@@ -11,7 +10,20 @@
 
   // handling votes
   const handleVote = (option, id) => {
-    dispatch("vote", { option, id });
+    PollStore.update(currentPolls => {
+      let copiedPolls = [...currentPolls];
+      let upvotedPoll = copiedPolls.find(poll => poll.id == id);
+
+      if (option === "a") {
+        upvotedPoll.votesA++;
+      }
+
+      if (option === "b") {
+        upvotedPoll.votesB++;
+      }
+
+      return copiedPolls;
+    });
   };
 </script>
 
