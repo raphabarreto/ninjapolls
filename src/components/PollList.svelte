cd60217cff7227835bb0b6cc7e275b451e9e58fa
Raphael Barreto <tnt.raphael@gmail.com>
2020-05-01 19:37:18 -0300

    Transition Basics

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,17 +1,8 @@
 <script>
-  // import { onMount, onDestroy } from "svelte";
+  import { fade, slide, scale } from "svelte/transition";
+  import { flip } from "svelte/animate";
   import PollStore from "../stores/PollStore.js";
   import PollDetails from "./PollDetails.svelte";
-
-  // export let polls = [];
-
-  // const unsub = PollStore.subscribe(data => {
-  //   polls = data;
-  // });
-
-  // onDestroy(() => {
-  //   unsub();
-  // });
 </script>
 
 <style>
@@ -24,7 +15,7 @@
 
 <div class="poll-list">
   {#each $PollStore as poll (poll.id)}
-    <div>
+    <div in:fade out:scale|local animate:flip={{ duration: 500 }}>
       <PollDetails {poll} />
     </div>
   {/each}
